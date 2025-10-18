e99038a46e57fc38ff2ce1e10b6746624089e6b8
ProCodrr <procodrr@gmail.com>
2025-10-18 22:40:24 +0530

    complete storageApp frontend

---
+++ services/s3.js
@@ -0,0 +1,74 @@
+import {
+  DeleteObjectCommand,
+  DeleteObjectsCommand,
+  GetObjectCommand,
+  HeadObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({ profile: "nodejs" });
+
+export const createUploadSignedUrl = async ({ key, contentType }) => {
+  const command = new PutObjectCommand({
+    Bucket: "procodrr-storage-app",
+    Key: key,
+    ContentType: contentType,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 300,
+    signableHeaders: new Set(["content-type"]),
+  });
+
+  return url;
+};
+
+export const createGetSignedUrl = async ({
+  key,
+  download = false,
+  filename,
+}) => {
+  const command = new GetObjectCommand({
+    Bucket: "procodrr-storage-app",
+    Key: key,
+    ResponseContentDisposition: `${download ? "attachment" : "inline"}; filename=${encodeURIComponent(filename)}`,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 300,
+  });
+
+  return url;
+};
+
+export const getS3FileMetaData = async (key) => {
+  const command = new HeadObjectCommand({
+    Bucket: "procodrr-storage-app",
+    Key: key,
+  });
+
+  return await s3Client.send(command);
+};
+
+export const deleteS3File = async (key) => {
+  const command = new DeleteObjectCommand({
+    Bucket: "procodrr-storage-app",
+    Key: key,
+  });
+
+  return await s3Client.send(command);
+};
+
+export const deleteS3Files = async (keys) => {
+  const command = new DeleteObjectsCommand({
+    Bucket: "procodrr-storage-app",
+    Delete: {
+      Objects: keys,
+      Quiet: false, // set true to skip individual delete responses
+    },
+  });
+
+  return await s3Client.send(command);
+};
