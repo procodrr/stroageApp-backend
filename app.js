f25c5f1ece0cc701e75dd1dc580d0b3b8cc9c77f
ProCodrr <procodrr@gmail.com>
2025-11-22 05:00:30 +0530

    change welcome message

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.post("/github-webhook", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from StorageApp!" });
+  res.json({ message: "Hello from My StorageApp!" });
 });
 
 app.get("/err", (req, res) => {
